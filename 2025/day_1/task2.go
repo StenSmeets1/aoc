3f750178e06c3658fa50e0f0f9f6e807af569827
Sten <stensmeets22@gmail.com>
2025-12-01 09:36:33 +0100

    day1 question2 also reformat question naming

---
+++ 2025/day_1/task2.go
@@ -0,0 +1,54 @@
+package main
+
+import ( 
+	"fmt"
+	"bufio"
+	"os"
+	"strconv"
+)
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	position := 50
+	zeroCount := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if len(line) == 0 {
+			continue
+		}
+		direction := line[0]
+		distance, err := strconv.Atoi(line[1:])
+		if err != nil {
+			panic(err)
+		}
+	
+		step := 1
+		if direction == 'L' {
+			step = -1
+		}
+
+		for i := 0; i < distance; i++ {
+			position = (position + step) % 100
+			if position < 0 {
+				position += 100
+			}
+
+			if position == 0 {
+				zeroCount++
+			}
+
+		}
+	}
+
+
+
+	fmt.Println("pass: ", zeroCount)
+}
