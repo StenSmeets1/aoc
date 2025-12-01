044d2d980d5b6efa46b6890c5cc8aa067dfff796
Sten <stensmeets22@gmail.com>
2025-12-01 09:25:41 +0100

    day1 question1

---
+++ 2025/day_1/day1.go
@@ -0,0 +1,50 @@
+package main
+
+import ( 
+	"fmt"
+	"bufio"
+	"os"
+	"strconv"
+)
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	position := 50
+	zeroCount := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if len(line) == 0 {
+			continue
+		}
+		direction := line[0]
+		distance, err := strconv.Atoi(line[1:])
+		if err != nil {
+			panic(err)
+		}
+	
+		if direction == 'L' {
+			position -= distance
+		} else if direction == 'R' {
+			position += distance
+		}
+		
+		position %= 100
+		if position < 0 {
+			position += 100
+		}
+
+		if position == 0 {
+			zeroCount++
+		}
+	}
+
+	fmt.Println("pass: ", zeroCount)
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Advent of Code
+
+These are my advent of code solutions in whatever language I'd like to use for that particular one. (Preferably not python though ;))
+
+
